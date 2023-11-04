7bf3e4f61f380631b834ca73d95b25a2060d7c57
NIMCOO <nimco77001@gmail.com>
2023-11-04 08:35:37 +0000

    DONE

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const pill =230
+ //calculate tip of the pill is between 50, 300
+ // the percentage is 15%
+
+ if( bill+tip ,230+34.5 ){
+   console. log("the pill is 230,and the tip is 34.5 and  total value =264.5")
+
+ }
